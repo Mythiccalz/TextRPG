58bd8c7cc7e457bd89a1cd51be0de426bf2ba676
Sergiuszz <sergiusz@sniec.pl>
2022-11-26 10:59:15 +0000

    added more possibilities in shop

--- RPGGame.py
+++ RPGGame.py
@@ -15,6 +15,7 @@ buydrink = ""
 boughtdrink = ""
 buysoup = ""
 boughtsoup = ""
+shopchoice = ""
 
 if move == "walk":
     print("you found "+str(dabloonrandomizer)+" dabloons!")
@@ -25,6 +26,38 @@ elif move == "shop":
     print("welcome to the shop!")
     print("what would you like?")
     print("you have no money! goodbye traveller! :]")
+    nextmove = input("what would you like to do? walk or end? :")
+    if nextmove == "walk":
+        print("you found "+str(dabloonrandomizer)+" dabloons!")
+        dabloons = dabloonsgot
+        print("you now have "+str(dabloons)+" dabloons!")
+        nextmove = input("what would you like to do? walk, shop or end? :")
+        if nextmove == "walk":
+            print("you found "+str(dabloonrandomizer)+" dabloons!")   
+        elif nextmove == "shop":
+            print("welcome to the shop!")
+            print(shop)
+            shopchoice = input("item :")
+            if shopchoice == "soup":
+                print("here you go! :]")
+                nextmove = input("what do you want to do? walk, items or end? :")
+                if nextmove == "walk":
+                    print("you found "+str(dabloonrandomizer)+" dabloons!")
+                    print("the possibilities end here for now!")
+                elif nextmove == "items":
+                    print("soup")
+                    nextmove = input("would you like to drink soup(y/n)? :")
+                    if nextmove == "y":
+                        print("you drank soup!")
+                        print("it does nothing")
+                        print("the possibilities end here for now!")
+                    elif nextmove == "n":
+                        print("you didnt drink soup")
+                        print("the possibilities end here for now!")
+                elif nextmove == "end":
+                    print("ok! be safe traveller :]")
+    elif nextmove == "end":
+        print("be safe traveller :)")
 elif move == "end":
     print("ok! be safe traveller :]")
 
